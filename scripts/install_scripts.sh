eea93b2663e96ef3b6a6f08896cc0e9a353add11
Alex Youngs <ayoungs@swri.org>
2018-08-28 19:45:18 -0500

    Added rule for add

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -4,4 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # installation
 cp $DIR/rgit /usr/local/bin/rgit
-cp $DIR/displays_off /usr/local/bin/displays_off
+
+cat <<EOF > /usr/local/bin/displays_off
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=`pgrep -a Xorg | cut -d " " -f 8`
+xset dpms force off
+EOF
+
+cat <<EOF > /usr/local/bin/displays_on
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=`pgrep -a Xorg | cut -d " " -f 8`
+xset dpms force on
+EOF
+
+cp 99-local.rules /etc/udev/rules.d/

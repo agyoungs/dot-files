e620f29b6e8d4716f8344cda6334b40d4df92bdc
Alex Youngs <ayoungs@swri.org>
2018-09-11 13:25:19 -0500

    Updated install script

--- setup_environment.sh
+++ setup_environment.sh
@@ -22,12 +22,6 @@ fi
 sed -i '/# Custom Settings (ayoungs)/Q' ~/.bashrc
 cat <<EOT >> ~/.bashrc
 # Custom Settings (ayoungs)
-export ROSDISTRO_INDEX_URL=https://ivs-git.dyn.datasys.swri.edu/external/rosdistro/raw/swri/index.yaml
-
-export CUDA_HOME=/usr/local/cuda
-export LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:\${CUDA_HOME}/lib64"
-PATH=\${CUDA_HOME}/bin:\${PATH}
-export PATH
 
 tmux_session=\`tmux display-message -p "#S" 2> /dev/null\` 
 if [ -f ~/workspaces/\$tmux_session/devel/setup.bash ]; then
